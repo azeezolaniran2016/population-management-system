b222ee85f1db0cf498295e1e8ad83dfaa5171644
Azeez Olaniran <azeezolaniran2016@gmail.com>
2018-03-25 16:34:27 +0100

    Implement data layer

---
+++ server/models/Location.js
@@ -0,0 +1,52 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Location = sequelize.define('Location', {
+    name:{ 
+      type: DataTypes.TEXT,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: {
+          msg: 'Location name cannot be empty',
+        }
+      }
+    },
+    femalePopulation: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Female population cannot be empty',
+        },
+        isNumeric: {
+          msg: 'Female population must be an integer',
+        },
+        min: {
+          args: [0],
+          msg: 'Minimum Female population cannot be less than 0'
+        }
+      },
+    },
+    malePopulation: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Male population cannot be empty',
+        },
+        isNumeric: {
+          msg: 'Male population must be an integer',
+        },
+        min: {
+          args: [0],
+          msg: 'Minimum Male population cannot be less than 0'
+        }
+      },
+    },
+    parentID: DataTypes.INTEGER
+  }, {});
+  Location.associate = function(models) {
+    Location.hasMany(models.Location, { as: 'SubLocation', foreignKey: 'parentID', })
+  };
+  return Location;
+};
